93c23ea1d9511230b564f06daba0e0641705a741
jgodoy1992 <juakogodoy@gmail.com>
2023-08-19 17:15:30 -0400

    Second commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,10 @@ export class LoginPage {
   username: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,private alertCtrl: AlertController) {}
 
   login() {
+    
     // Validar aquí los datos ingresados
     if (this.username.length >= 3 && this.username.length <= 8 && /^\d{4}$/.test(this.password)) {
       // Pasar los datos a la página Home
@@ -20,8 +22,19 @@ export class LoginPage {
         state: { username: this.username },
       });
     } else {
+      this.mostrarAlerta()
       // Mostrar mensaje de error
       console.log('Error en los datos ingresados.');
     }
   }
+
+  async mostrarAlerta(){
+    const alert = await this.alertCtrl.create({
+      message: `Usuario entre 3 y 8 caracteres y contraseña de 4 digitos`,
+      buttons: ['OK'],
+    });
+    await alert.present()
+  }
+
+  
 }
